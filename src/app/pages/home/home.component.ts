e0031f299ecfbfaf608e4d450702c38251308b06
FILÂTRE Killian <k.filatre@gmail.com>
2025-03-22 00:24:54 +0100

    setup bootstrap and chartjs datalabels

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,31 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { OlympicService } from 'src/app/core/services/olympic.service';
-import { ChartOptions, ChartType, ChartData } from 'chart.js';
-import { OlympicCountry } from 'src/app/core/models/Olympic';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { OlympicService } from "src/app/core/services/olympic.service";
+import { Chart, ChartData, ChartOptions, ChartType } from "chart.js";
+import { OlympicCountry } from "src/app/core/models/Olympic";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+
+Chart.register(ChartDataLabels);
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
   standalone: false,
 })
+
 export class HomeComponent implements OnInit {
   public olympics$: Observable<OlympicCountry[]> | null = null;
+  public totalMedals: number = 0;
 
   public pieChartOptions: ChartOptions = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top',
+        display: true,
+        position: "bottom",
+      },
+      tooltip: {
+        enabled: true,
+      },
+      datalabels: {
+        display: true,
+        color: "#FFFFFF",
+        font: {
+          size: 14,
+          weight: "bold",
+        },
+        formatter: (value: number, context: any) => {
+          return context.chart.data.labels[context.dataIndex];
+        },
       },
     },
   };
 
-  public pieChartType: ChartType = 'pie';
+  public pieChartType: ChartType = "pie";
 
-  public pieChartData: ChartData<'pie', number[], string | string[]> = {
+  public pieChartData: ChartData<"pie", number[], string | string[]> = {
     labels: [],
     datasets: [
       {
@@ -37,12 +57,6 @@ export class HomeComponent implements OnInit {
 
   constructor(private olympicService: OlympicService) {}
 
-
-  /**
-   * Initializes the component by Olympic data from the service
-   *
-   * @return {void}
-   */
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
 
@@ -51,7 +65,6 @@ export class HomeComponent implements OnInit {
         map((olympics: OlympicCountry[]) => {
           if (olympics?.length > 0) {
             this.pieChartData.labels = olympics.map((o) => o.country);
-
             this.pieChartData.datasets[0].data = olympics.map((o) =>
               o.participations.reduce(
                 (total: number, p: any) => total + p.medalsCount,
@@ -59,31 +72,37 @@ export class HomeComponent implements OnInit {
               )
             );
 
+            this.totalMedals = olympics.reduce(
+              (total: number, country: OlympicCountry) =>
+                total + country.participations.reduce((sum: number, p: any) =>
+                  sum + p.medalsCount, 0), 0
+            );
+
             // Background-color
             this.pieChartData.datasets[0].backgroundColor = [
-              '#9e5c63',
-              '#823952',
-              '#82a2e0',
-              '#9b7fa3',
-              '#b3cbea',
+              "#9e5c63",
+              "#b3cbea",
+              "#82a2e0",
+              "#823952",
+              "#9b7fa3",
             ];
 
             // Hover:Background-color
             this.pieChartData.datasets[0].hoverBackgroundColor = [
-              '#b97a80',
-              '#9e5e70',
-              '#9bb3ea',
-              '#af97b4',
-              '#c6d6ef',
+              "#b97a80",
+              "#c6d6ef",
+              "#9bb3ea",
+              "#9e5e70",
+              "#af97b4",
             ];
 
             // Hover:Border-color
             this.pieChartData.datasets[0].hoverBorderColor = [
-              '#b97a80',
-              '#9e5e70',
-              '#9bb3ea',
-              '#af97b4',
-              '#c6d6ef',
+              "#b97a80",
+              "#c6d6ef",
+              "#9bb3ea",
+              "#9e5e70",
+              "#af97b4",
             ];
           }
         })
