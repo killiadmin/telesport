63f94cad34ed67aec75be6f56eb466d2996dc718
FILÂTRE Killian <k.filatre@gmail.com>
2025-03-21 11:53:19 +0100

    setup package ng2 charts + try pie charts for olympic games data

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -1,9 +1,5 @@
-// TODO: create here a typescript interface for an olympic country
-/*
-example of an olympic country:
-{
-    id: 1,
-    country: "Italy",
-    participations: []
+export interface OlympicCountry {
+    id: number;
+    country: string;
+    participations: any[];
 }
-*/
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,93 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { ChartOptions, ChartType, ChartData } from 'chart.js';
+import { OlympicCountry } from 'src/app/core/models/Olympic';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
-    standalone: false
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  standalone: false,
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics$: Observable<OlympicCountry[]> | null = null;
+
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+    },
+  };
+
+  public pieChartType: ChartType = 'pie';
+
+  public pieChartData: ChartData<'pie', number[], string | string[]> = {
+    labels: [],
+    datasets: [
+      {
+        data: [],
+        backgroundColor: [],
+      },
+    ],
+  };
 
   constructor(private olympicService: OlympicService) {}
 
+
+  /**
+   * Initializes the component by Olympic data from the service
+   *
+   * @return {void}
+   */
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+
+    this.olympics$
+      .pipe(
+        map((olympics: OlympicCountry[]) => {
+          if (olympics?.length > 0) {
+            this.pieChartData.labels = olympics.map((o) => o.country);
+
+            this.pieChartData.datasets[0].data = olympics.map((o) =>
+              o.participations.reduce(
+                (total: number, p: any) => total + p.medalsCount,
+                0
+              )
+            );
+
+            // Background-color
+            this.pieChartData.datasets[0].backgroundColor = [
+              '#9e5c63',
+              '#823952',
+              '#82a2e0',
+              '#9b7fa3',
+              '#b3cbea',
+            ];
+
+            // Hover:Background-color
+            this.pieChartData.datasets[0].hoverBackgroundColor = [
+              '#b97a80',
+              '#9e5e70',
+              '#9bb3ea',
+              '#af97b4',
+              '#c6d6ef',
+            ];
+
+            // Hover:Border-color
+            this.pieChartData.datasets[0].hoverBorderColor = [
+              '#b97a80',
+              '#9e5e70',
+              '#9bb3ea',
+              '#af97b4',
+              '#c6d6ef',
+            ];
+          }
+        })
+      )
+      .subscribe();
   }
 }
